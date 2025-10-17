00dc50ecdbf659fb16032a5152f40143459c2c24
kate_babych <Babych_k@i.ua>
2025-10-17 09:23:19 +0300

    Refactor  CandidatesUserControl

--- Hirely.UI/Views/CandidatesUserControl.xaml.cs
+++ Hirely.UI/Views/CandidatesUserControl.xaml.cs
@@ -1,53 +1,38 @@
 ﻿using Hirely.UI.ViewModels;
 using System.Collections.ObjectModel;
-using System.ComponentModel;
 using System.Windows;
 using System.Windows.Controls;
 
 namespace Hirely.UI.Views
 {
-    public partial class CandidatesUserControl : UserControl, INotifyPropertyChanged
+    public partial class CandidatesUserControl : UserControl
     {
         public CandidatesUserControl()
         {
             InitializeComponent();
         }
-
-        public event PropertyChangedEventHandler PropertyChanged;
-        protected void OnPropertyChanged(string name) =>
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
-
-        // Колекція кандидатів
+        public ObservableCollection<CandidateViewModel> Candidates
+        {
+            get => (ObservableCollection<CandidateViewModel>)GetValue(CandidatesProperty);
+            set => SetValue(CandidatesProperty, value);
+        }
         public static readonly DependencyProperty CandidatesProperty =
             DependencyProperty.Register(nameof(Candidates),
                 typeof(ObservableCollection<CandidateViewModel>),
                 typeof(CandidatesUserControl),
                 new PropertyMetadata(null));
 
-        public ObservableCollection<CandidateViewModel> Candidates
+        public CandidateViewModel SelectedCandidate
         {
-            get => (ObservableCollection<CandidateViewModel>)GetValue(CandidatesProperty);
-            set
-            {
-                SetValue(CandidatesProperty, value);
-                OnPropertyChanged(nameof(Candidates));
-            }
+            get => (CandidateViewModel)GetValue(SelectedCandidateProperty);
+            set => SetValue(SelectedCandidateProperty, value);
         }
 
         public static readonly DependencyProperty SelectedCandidateProperty =
             DependencyProperty.Register(nameof(SelectedCandidate),
                 typeof(CandidateViewModel),
                 typeof(CandidatesUserControl),
-                new PropertyMetadata(null));
+                new FrameworkPropertyMetadata(null, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
 
-        public CandidateViewModel SelectedCandidate
-        {
-            get => (CandidateViewModel)GetValue(SelectedCandidateProperty);
-            set
-            {
-                SetValue(SelectedCandidateProperty, value);
-                OnPropertyChanged(nameof(SelectedCandidate));
-            }
-        }
     }
 }
