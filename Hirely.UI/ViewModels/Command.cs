2d9c3cc2646215ba1e0e6251a02acdca04f9c676
kate_babych <Babych_k@i.ua>
2025-10-17 08:13:09 +0300

    Refactor ICommand

--- Hirely.UI/ViewModels/Command.cs
+++ Hirely.UI/ViewModels/Command.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Input;
+﻿using System.Windows.Input;
 
 namespace Hirely.UI.ViewModels
 {
@@ -25,5 +20,4 @@ namespace Hirely.UI.ViewModels
         public event EventHandler CanExecuteChanged;
         public void RaiseCanExecuteChanged() => CanExecuteChanged?.Invoke(this, EventArgs.Empty);
     }
-
 }
